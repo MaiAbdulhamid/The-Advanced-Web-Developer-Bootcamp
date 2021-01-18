280809fbc783214b069fece50dc2f3cd06babca0
Mai Abdulhamid <mai.abdelhameed95@gmail.com>
2021-01-18 14:15:10 +0200

    some changes

--- projects/2. Creating JSON APIs With Node and Mongo/models/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.set('debug', true)
-mongoose.connect('mongodb://localhost:27017/tododb', {useNewUrlParser: true, useUnifiedTopology: true})
-mongoose.Promise = Promise;
-
-module.exports.Todo = require('./todo')
