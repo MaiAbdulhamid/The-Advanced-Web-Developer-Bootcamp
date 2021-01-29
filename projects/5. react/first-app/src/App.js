baaf1c524951736afb144a3e2e6dd4df12c05ade
Mai Abdulhamid <mai.abdelhameed95@gmail.com>
2021-01-29 21:27:11 +0200

    add project

---
+++ projects/5. react/first-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import RecipeList from "./RecipeList";
+import Navbar from './Navbar'
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <RecipeList />
+    </div>
+  );
+}
+
+export default App;
