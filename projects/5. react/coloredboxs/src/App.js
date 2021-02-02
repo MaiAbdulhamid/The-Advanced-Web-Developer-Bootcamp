f36a0ec3e4fa036c5eee9a76cd46ff5729049349
Mai Abdulhamid <mai.abdelhameed95@gmail.com>
2021-02-02 17:19:21 +0200

    add projects

---
+++ projects/5. react/coloredboxs/src/App.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react';
+import './App.css';
+const NUM_BOXS = 32;
+
+const Box = ({color}) => {
+  const style = {
+    backgroundColor: color,
+    width: '180px',
+    height: '180px',
+    display: 'inline-block'
+  }
+
+  return <div style={style} />
+}
+class App extends Component {
+  
+  constructor(props) {
+    super(props);
+    const boxs = Array(NUM_BOXS).fill().map(this.getRandColor, this)
+    this.state = {boxs: boxs}
+    setInterval(() => {
+      const boxs = this.state.boxs.slice()
+      const randIndx = Math.floor(Math.random() * boxs.length)
+      boxs[randIndx] = this.getRandColor();
+      this.setState({boxs})
+    }, 400)
+  }
+  getRandColor(){
+    let randIndex = Math.floor(Math.random() * this.props.allColors.length)
+    return this.props.allColors[randIndex]
+  }
+  render() {
+    const boxs = this.state.boxs.map((color, i) => {
+      return <Box key={i} color={color} />
+    })
+    return (
+      <div className="App">
+        {boxs}
+      </div>
+    );
+  }
+}
+
+App.defaultProps = {
+  allColors: ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond",
+              "Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate",
+              "Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod",
+              "DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange",
+              "DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey",
+              "DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue",
+              "FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod",
+              "Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki",
+              "Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan",
+              "LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon",
+              "LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow",
+              "Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid",
+              "MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise",
+              "MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy",
+              "OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen",
+              "PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue",
+              "Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen",
+              "SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen",
+              "SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke",
+              "Yellow","YellowGreen"]
+};
+
+export default App;
--- projects/5. react/first-app/src/App.js
+++ projects/5. react/first-app/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import RecipeList from "./RecipeList";
 import Navbar from './Navbar'
+import Instructors from './Instructors'
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <RecipeList />
+        <Instructors />
     </div>
   );
 }
